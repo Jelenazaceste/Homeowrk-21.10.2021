0788310f7837e5e35206f7553ed59dba044f3553
Jelenazaceste <jel.zaceste@gmail.com>
2021-10-21 18:52:44 +0300

    Add project files.

---
+++ Homeowrk 21.10.2021/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeowrk_21._10._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 10, 219, 34, 145, 510 };
+            int sum = 0;
+            foreach (int number in intArray)
+            { sum += number; }
+
+            {
+                Console.WriteLine(sum);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
